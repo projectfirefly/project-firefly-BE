c2171383e2d255cdde1a04c12fccfb1ab5dab537
Robert Driskell <driskell.ba@gmail.com>
2019-07-30 11:55:31 -0500

    Tied kids to users

--- models/kidModel.js
+++ models/kidModel.js
@@ -1,27 +1,33 @@
 const db = require("../data/dbConfig.js");
 
 module.exports = {
-  findById,
-  findBy,
-  getAll,
-  addKid
+    findById,
+    findBy,
+    getAll,
+    addKid,
+    findByUserId
 };
 
 function findById(id) {
-  return db("kids")
-    .where({ id })
-    .first();
+    return db("kids")
+        .where({ id })
+        .first();
 }
 
 function findBy(filter) {
-  return db("kids").where(filter);
+    return db("kids").where(filter);
 }
 
 function getAll() {
-  return db("kids");
+    return db("kids");
+}
+
+function findByUserId(user_id) {
+    return db("kids")
+        .where({user_id});
 }
 
 async function addKid(kid) {
-  const [id] = await db("kids").insert(kid, "id");
-  return findById(id);
+    const [id] = await db("kids").insert(kid, "id");
+    return findById(id);
 }
